ba0abaa862c1f11219b2ac26fe59ca943a4f6f6d
seyammir <seyam.general@gmail.com>
2025-12-14 20:07:33 +0100

    add main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def main():
+    st.title('Device Management')
+    st.write('Welcome to the Device Management Application!')
+
+    st.sidebar.title('Navigation')
+    st.sidebar.info('Use the sidebar to navigate through different sections.')
+
+if __name__ == '__main__':
+    main()
